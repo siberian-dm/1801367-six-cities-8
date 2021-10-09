967a16e5b2bb03a019fde9ec402d61080b28987d
Dmitry Vorobev <dm.siberian@gmail.com>
2021-10-09 18:26:04 +0300

    feat: add mocks generate function

---
+++ project/src/mock/offers.ts
@@ -0,0 +1,32 @@
+import faker from 'faker';
+import { Hotel } from '../types/hotel';
+
+function hotelMock() {
+  return {
+    id: faker.datatype.uuid(),
+    isPremium: faker.datatype.boolean(),
+    previewImage: faker.helpers.randomize([
+      'img/apartment-01.jpg',
+      'img/apartment-02.jpg',
+      'img/apartment-03.jpg',
+      'img/room.jpg',
+    ]),
+    price: faker.datatype.number({min: 80, max: 200}),
+    isFavorite: faker.datatype.boolean(),
+    rating: faker.datatype.number({min: 10, max: 100, precision: 10}),
+    title: faker.helpers.randomize([
+      'Beautiful &amp; luxurious apartment at great location',
+      'Wood and stone place',
+      'Canal View Prinsengracht',
+      'Nice, cozy, warm big bed apartment',
+    ]),
+    type: faker.helpers.randomize([
+      'Private room',
+      'Apartment',
+    ]),
+  };
+}
+
+export function generateOffers(count: number): Hotel[] {
+  return new Array(count).fill(null).map(() => hotelMock());
+}
