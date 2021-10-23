75a609b819129e1f84660b67efde18db09f888ed
Dmitry Vorobev <dm.siberian@gmail.com>
2021-10-23 16:10:00 +0300

    refactor: add constants for calculateRating function

--- project/src/utils.ts
+++ project/src/utils.ts
@@ -1,7 +1,10 @@
 import dayjs from 'dayjs';
 import { DateFormat } from './const';
 
-export const calculateRating = (rating: number): string => `${rating / 5 * 100}%`;
+const MAX_STARS = 5;
+const MAX_RATING = 100;
+
+export const calculateRating = (rating: number): string => `${rating / MAX_STARS * MAX_RATING}%`;
 
 export const capitalizeString = (string: string): string => `${string[0].toUpperCase()}${string.slice(1).toLowerCase()}`;
 
